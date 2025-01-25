899d05c4c56c59da7c519c3c5bcefd5558e895ce
dinesh-g1 <dineshgillalaa@gmail.com>
2025-01-25 15:18:25 +0530

    feat: open a file and count words frequency

---
+++ maps/fileandmap.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	f, err := os.Open("ramayan.txt")
+	if err != nil {
+		log.Printf("can't open the file. err : %v", err)
+	}
+	words, _ := wordFrequency(f)
+	for k, v := range words {
+		fmt.Println(k, "-", v)
+	}
+}
+
+func wordFrequency(content io.Reader) (map[string]int, error) {
+	sc := bufio.NewScanner(content)
+	wordFreq := make(map[string]int)
+	for sc.Scan() {
+		line := sc.Text()
+		line = strings.ToLower(line)
+		words := strings.Split(line, " ")
+
+		for _, w := range words {
+			wordFreq[w] = 1
+		}
+	}
+
+	return wordFreq, nil
+}
