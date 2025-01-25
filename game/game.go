f1582199c61938320053e770a49a9a13c4e6806d
dinesh-g1 <dineshgillalaa@gmail.com>
2025-01-25 11:48:28 +0530

    feat: type switch, embedding

--- game/game.go
+++ game/game.go
@@ -6,22 +6,74 @@ import (
 
 func main() {
 	i := NewItem(10, 20)
-	p1 := Person{Name: "dinesh", Item: NewItem(10,20)}
+	p1 := Player{Name: "dinesh", Item: NewItem(10, 20)}
 
 	movers := []Mover{&i, &p1}
-
-	for i, m := range movers {
-		fmt.Println(m.Move(i*100,i*200))
+	for _, v := range movers {
+		switch v.(type) {
+		case *Item:
+			fmt.Println("Item type", v.(*Item).X)
+		case *Player:
+			fmt.Println("Player type", v.(*Player).Keys)
+		default:
+			fmt.Printf("unknown %T\n", v)
+		}
 	}
 }
 
-type Mover interface{
-	Move(int , int) string
+type Mover interface {
+	Move(int, int)
 }
 
-type Person struct {
+type Player struct {
 	Item
 	Name string
+	Keys []Key
+}
+
+type Key byte
+
+const (
+	Bangaram = iota + 1 //Gold
+	Vendi               //Silver
+	Ithhadi             //Bronze
+	Raagi               //Raagi
+	invalidKey
+)
+
+func (k Key) String() string {
+	switch k {
+	case Bangaram:
+		return "Gold"
+	case Vendi:
+		return "Silver"
+	case Ithhadi:
+		return "Bronze"
+	case Raagi:
+		return "Raagi"
+	}
+	return fmt.Sprintf("invalid key %d", k)
+}
+
+func (p *Player) FoundKey(k Key) error {
+	if k < Bangaram || k >= invalidKey {
+		return fmt.Errorf("invalid key %d", k)
+	}
+
+	if !containsKey(p.Keys, k) {
+		p.Keys = append(p.Keys, k)
+	}
+
+	return nil
+}
+
+func containsKey(keys []Key, k Key) bool {
+	for _, k1 := range keys {
+		if k1 == k {
+			return true
+		}
+	}
+	return false
 }
 
 type Item struct {
@@ -36,10 +88,7 @@ func NewItem(x, y int) Item {
 	}
 }
 
-func (i *Item) Move(x, y int) string {
+func (i *Item) Move(x, y int) {
 	i.X = x
 	i.Y = y
-
-	return "moved to " + string(x) + " and " + string(y)
 }
-
