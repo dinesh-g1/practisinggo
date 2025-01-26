a04effc1b532ada6a793899087c5cb0c60738053
dinesh-g1 <dineshgillalaa@gmail.com>
2025-01-26 13:29:15 +0530

    feat: channels def, usage

--- channels/channel.go
+++ channels/channel.go
@@ -2,18 +2,27 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
 	go fmt.Println("go routing definition") //Spinning up goroutine with named function
-	fmt.Println("main")
 
 	for i := 0; i < 5; i++ {
-		i := i //Shadowing the i before using by goroutine
-		go func () {			//anonymous functioin go routine
+		i := i      //Shadowing the i before using by goroutine
+		go func() { //anonymous functioin go routine
 			fmt.Println(i)
 		}()
 	}
-	time.Sleep(time.Second*2)
+
+	strChan := make(chan string) //create a channel with a type
+	go func() {
+		for i := 0; i < 5; i++ {
+			strChan <- fmt.Sprintf("val%d", i+1)
+		}
+		close(strChan)
+	}()
+ 
+	for v := range strChan {
+		fmt.Println(v)
+	}
 }
